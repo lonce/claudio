894b6661485aee168fa132a7488f43e3d0fd8a28
lonce <lonce.acad@whome.org>
2024-09-25 16:35:15 +0200

    string param test

--- www/src/models/DroneModel.js
+++ www/src/models/DroneModel.js
@@ -59,16 +59,12 @@ export class DroneModel extends BaseSound {
         } else if (name === 'gain') {
             this.gainNode.gain.setTargetAtTime(param.get(), this.context.currentTime, param.attackTime);
             console.log(`updateParameter, gain = ${param.get()}`)
-        } 
-    }
-
-    updateStringParameter(name) {
-        const param = this.getStringParameter(name);
-        if (name === 'waveform' && this.oscillator) {
+        } else if (name === 'waveform' && this.oscillator) {
             this.oscillator.type = param.get();
             console.log(`updateParameter, waveform = ${param.get()}`)   
         }
     }
+
 
 
     destroy() {
